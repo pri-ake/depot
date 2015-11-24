420da5d4393f8340936b1e75eba6c1b01c914060
pri-ake <chiran.cha.no.1@gmail.com>
2015-11-24 06:39:32 +0000

    add I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :now
   before_action :authorize
+  before_action :set_i18n_locale_from_params
+
 
   def now
-    @time = Time.now
+    @time = Time.zone.now
   end
 
   private
@@ -24,6 +26,22 @@ class ApplicationController < ActionController::Base
       cart = Cart.create
       session[:cart_id] = cart.id
       cart
+    end
+
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.include?(params[:locale].to_sym)
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] =
+          "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+
+    def default_url_options
+      {locale: I18n.locale}
     end
 
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,13 @@ class StoreController < ApplicationController
     skip_before_action :authorize
 
   def index
+    if params[:set_locale]
+      redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+      @cart = current_cart
+    end
+
     @products = Product.order(:title)
 
     #デバッグ用
