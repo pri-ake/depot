5909dbe6416fd9c7a7442ae404c41df88bb761e7
pri-ake <chiran.cha.no.1@gmail.com>
2015-11-24 00:42:34 +0000

    User controller Session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :now
+  before_action :authorize
 
   def now
     @time = Time.now
   end
 
   private
+
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "ログインしてください"
+      end
+    end
+
 
     def current_cart
       Cart.find(session[:cart_id])
